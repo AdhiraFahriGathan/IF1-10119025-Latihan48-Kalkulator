854226c1329d5893748e1e68d19ead353d927714
unknown <adhiragathan12@gmail.com>
2020-11-14 10:45:32 +0700

    initial commit

---
+++ src/if1/pkg10119025/latihan48/kalkulator/IF110119025Latihan48Kalkulator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119025.latihan48.kalkulator;
+
+/**
+ *
+ * @author User
+ * * NAMA     : ADHIRA FAHRI GATHAN
+ * KELAS    : PBO
+ * NIM      : 10119025
+ * Deskripsi Program : Program ini berisi tentang Nilai Kalkulator
+ */
+public class IF110119025Latihan48Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+        Kalkulator kalkulator = new Kalkulator();
+        kalkulator.setValue1(7);
+        kalkulator.setValue2(5);
+        kalkulator.setNameProject();
+        kalkulator.setNoteProject("This project shown you how to manage method in java");
+        System.out.println("Result Add is = " + kalkulator.add(kalkulator.getValue1(), kalkulator.getValue2()));
+        System.out.println("Result Minus is = " + kalkulator.minus(kalkulator.getValue1(), kalkulator.getValue2()));
+        System.out.println("Result Multiple is = " + kalkulator.multiplication(kalkulator.getValue1(), kalkulator.getValue2()));
+        System.out.println("Result Division is = " + kalkulator.division(kalkulator.getValue1(), kalkulator.getValue2()));
+    }
+}
